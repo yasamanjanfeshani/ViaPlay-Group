aa509bea54ce8d9252c1583f3978326208a44c43
yasaman <y.janfeshani@gmail.com>
2023-02-19 20:51:38 +0330

    add comment

--- src/components/movieList/index.js
+++ src/components/movieList/index.js
@@ -4,17 +4,16 @@ import MovieItem from "../movieItem";
 const MovieList = ({ callBackMovieDetail, products, selected }) => {
   return (
     <div className={styles.listMovie}>
-      {products &&
-        products.map((value, index) => {
-          return (
-            <MovieItem
-              data={value}
-              key={index}
-              callBackMovieDetail={(data) => callBackMovieDetail(data)}
-              selected={selected}
-            />
-          );
-        })}
+      {products.map((value, index) => {
+        return (
+          <MovieItem
+            data={value}
+            key={index}
+            callBackMovieDetail={(data) => callBackMovieDetail(data)}
+            selected={selected}
+          />
+        );
+      })}
     </div>
   );
 };
